9591e4e17c4fbf914710cffdca33b1453d967aa2
Parmeet Singh <parmeetmander90@gmail.COM>
2015-04-17 12:09:31 -0400

    index and search api works

--- cinewow/app/controllers/omdb_controller.rb
+++ cinewow/app/controllers/omdb_controller.rb
@@ -1,6 +1,13 @@
 class OmdbController < ApplicationController
 
 	def index
+		@response = HTTParty.get("http://www.omdbapi.com/")
+	end
 
+	def show
+		title = params[:title].split.join("%20")
+		year = params[:year]
+		@response = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{year}")
+		render :result
 	end
 end
--- cinewow/config/routes.rb
+++ cinewow/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'omdb#index'
+  get '/search', to: 'omdb#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
